25bcae8489286278b1b05f88c52ae7226c0c1bec
Shahzad A. Chaudhry <shahzad_chaudhry@hotmail.com>
2018-08-07 20:48:23 +0100

    Updated to v6.3.2

--- deployStack.sh
+++ deployStack.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker network create --driver overlay elastic
-ELASTIC_VERSION=6.3.1 docker stack deploy -c docker-compose.yml elastic
+ELASTIC_VERSION=6.3.2 docker stack deploy -c docker-compose.yml elastic
